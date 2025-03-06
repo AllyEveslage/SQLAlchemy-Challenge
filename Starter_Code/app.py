070221a5d334f138bfd71557009ab42010453b0a
Ally E <dv4218bx@go.minnstate.edu>
2025-03-05 18:47:18 -0600

    first part of flask code working

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,6 +1,13 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, and_
 
+from flask import Flask, jsonify
 
 #################################################
 # Database Setup
@@ -8,25 +15,39 @@
 
 # Create engine using the `hawaii.sqlite` database file
 
+engine = create_engine("sqlite:///C:/Users/12182/OneDrive/Desktop/class-materials/UofM-VIRT-DATA-PT-12-2024-U-LOLC-MWTH/SQLAlchemy-Challenge/Starter_Code/Resources/hawaii.sqlite")
+
 # Declare a Base using `automap_base()`
+
+Base = automap_base()
 
 # Use the Base class to reflect the database tables
 
+Base.prepare(autoload_with=engine)
 
 # Assign the measurement class to a variable called `Measurement` and
 # the station class to a variable called `Station`
 
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create a session
-
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
-
+app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+@app.route('/')
+def starter():
+    """Listing available API Routes"""
+    return(
+        f'Available Routes:<br/>'
+        f'/api/v1.0/precipitation<br/>'
+        f'/api/v1.0/stations<br/>'
+        f'/api/v1.0/tobs<br/>'
+        f'/api/v1.0/<start><br/>'
+        f'/api/v1.0/<start>/<end>'
+    )
